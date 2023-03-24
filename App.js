0a9b07b42fc5dd37b0191c19dca238e47d578c1d
Akib <ahabole@deloitte.com>
2023-03-24 12:58:34 +0530

    saprate index.html in to app.js

--- App.js
+++ App.js
@@ -1,21 +1,16 @@
-
-{/* <div id="parent">
-    <div id = "child">
-        <h1>I am h1 new one</h1>
-        <h2> I am h2 new one
-    </div>
-</div> */}
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const heading = React.createElement(
     "div",
     {id:"parent"},
    [
     React.createElement("div",{id:"child1"},
-    [React.createElement("h1",{},"I am h1 new one"),
+    [React.createElement("h1",{},"I am child1 h1 new one"),
     React.createElement("h2",{},"I am h2 new one")
     ]),
 React.createElement("div",{id:"child2"},
-[React.createElement("h1",{},"I am h1 new one"),
+[React.createElement("h1",{},"I am child2 h1 new one"),
 React.createElement("h2",{},"I am h2 new one")
 ])
    ]);
